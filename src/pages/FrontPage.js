60f7029826aa0460f6a0d58004a7933950c95fe2
unknown <lucasdafonsecadepaula@gmail.com>
2021-12-23 23:57:44 -0300

    Testando o Sass

---
+++ src/pages/FrontPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FrontPage.scss';
+
+export default function FrontPage() {
+  return (
+    <div>
+      <h1 className="text">TESTING SASS</h1>
+    </div>
+  );
+}
